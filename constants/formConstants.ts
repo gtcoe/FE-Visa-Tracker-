7874060df96bcc6ac3718396b1656f3c349e3606
Garvit Tyagi <garvittyagicoe@gmail.com>
2025-03-23 22:35:25 +0530

    d

---
+++ constants/formConstants.ts
@@ -0,0 +1,23 @@
+// Form modes for FillServiceForm
+export enum FORM_MODE {
+  VIEW = 'view',
+  EDIT = 'edit',
+  ADD_SUB_REQUEST = 'add-sub-request'
+}
+
+// Tab names for CommonScreen
+export enum TAB_NAME {
+  SEARCH = 'search',
+  FILL = 'fill',
+  SUMMARY = 'summary'
+}
+
+// LocalStorage keys
+export enum STORAGE_KEY {
+  FORM_MODE = 'formMode',
+  APPLICATION_ID = 'applicationId',
+  ACTIVE_TAB = 'activeTab',
+  SERVICE_REFERENCE_NUMBER = 'serviceReferenceNumber',
+  APPLICATION_INFO = 'applicationInfo',
+  USER_ID = 'userId'
+} 
