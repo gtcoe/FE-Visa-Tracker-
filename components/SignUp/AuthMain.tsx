acf6c8f6a15ddd5179d44dfb1bb46341d4873ad9
Garvit Tyagi <garvittyagicoe@gmail.com>
2025-04-01 22:26:33 +0530

    d

--- components/SignUp/AuthMain.tsx
+++ components/SignUp/AuthMain.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import SignUpAuthentication from "./SignUpAuthentication";
 import Image from "next/image";
 import SignUpImg from "../../public/SignUpImg.svg";
+import VisaApprovedStamp from "../Assets/Images/visaApprovedStamp.svg";
 
 const AuthPage = () => {
   return (
@@ -10,6 +11,10 @@ const AuthPage = () => {
       <div className="w-1/2 h-full flex justify-center items-center ml-20">
         <div className="w-[64%] h-[100vh] absolute left-[1%]">
           <Image src={SignUpImg} alt="sign-up-bg" fill />
+        </div>
+        {/* Visa Approved Stamp */}
+        <div className="absolute left-[10%] top-[29%] w-[150px] h-[150px] z-10">
+          <Image src={VisaApprovedStamp} alt="visa-approved-stamp" width={150} height={150} />
         </div>
       </div>
       <div className="w-1/2 flex items-center justify-items-start z-[1000]">
